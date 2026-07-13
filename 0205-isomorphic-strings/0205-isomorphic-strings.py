37d8fec6f1813971c4c69437c8220c3cd8605997
MaheshBabuDevarakonda <188674814+MaheshBabuDevarakonda@users.noreply.github.com>
2026-07-13 17:21:50 +0530

    Time: 5 ms (48.61%), Space: 19.2 MB (46.34%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dici={}
+        rev={}
+        iso=True
+        for i in range(len(s)):
+            ch1=s[i]
+            ch2=t[i]
+            if ch1 not in dici and ch2 not in rev:
+                dici[ch1]=ch2
+                rev[ch2]=ch1
+            elif ch1 in dici and dici[ch1]!=ch2:
+                iso=False
+                break
+            elif ch2 in rev and rev[ch2]!=ch1:
+                iso=False
+                break
+        return iso
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

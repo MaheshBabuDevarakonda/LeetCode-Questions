1e2b8cafa58565149779a8610a49dea75b674499
MaheshBabuDevarakonda <188674814+MaheshBabuDevarakonda@users.noreply.github.com>
2026-07-30 18:32:50 +0530

    Time: 5 ms (59.28%), Space: 26.4 MB (93.31%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        k=k%n
+
+        self.rev(nums,0,n-1)
+        self.rev(nums,0,k-1)
+        self.rev(nums,k,n-1)
+
+
+
+    def rev(self,nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

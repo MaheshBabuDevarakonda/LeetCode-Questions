5feb410b80abf37131efbabc77e0a9a47ce060e0
MaheshBabuDevarakonda <188674814+MaheshBabuDevarakonda@users.noreply.github.com>
2026-07-30 11:06:20 +0530

    Added solution - Time: O(n^2), Space: O(1)

---
+++ 700148-insertion-sort-GFG/700148-insertion-sort.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insertionSort(self, arr):
+        # code here
+        n=len(arr)
+        for i in range(1,n):
+            key=arr[i]
+            j=i-1
+            while(j>=0 and  arr[j]>key):
+                arr[j+1]=arr[j]
+                j-=1
+            arr[j+1]=key
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

33ff511fb0885f7e1608a7f27a34344e4ccbb5d7
devarakondamaheshbabu <188674814+MaheshBabuDevarakonda@users.noreply.github.com>
2026-09-25 23:03:43 +0530

    Time: 7 ms (3.85%), Space: 19.3 MB (64.69%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")":"(", "}":"{", "]":"["}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        
+        return not stack
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
